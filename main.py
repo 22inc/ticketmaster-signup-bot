9857da51dc4bab6156012dd58dcd358716e9a581
22inc <136368985+22inc@users.noreply.github.com>
2023-11-06 13:59:04 -0800

    Update.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import time
 import chromedriver_autoinstaller
-import selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 chromedriver_autoinstaller.install()
 
@@ -28,7 +26,6 @@ pBillingState = ""
 pBillingPhone = ""
 
 driver = ""
-
 ticketmasterAccountList = []
 
 def profileSelect():
@@ -50,30 +47,12 @@ def profileSelect():
             # Define shipping variables from the selected profile.
             if "Shipping Address:" in line:
                 pShippingAddress = line.split("Shipping Address: ")[1].strip()
-            if "Shipping Secondary:" in line:
-                pShippingSecondary = line.split("Shipping Secondary: ")[1].strip()
-            if "Shipping City:" in line:
-                pShippingCity = line.split("Shipping City: ")[1].strip()
-            if "Shipping Zip Code:" in line:
-                pShippingZipCode = line.split("Shipping Zip Code: ")[1].strip()
-            if "Shipping State:" in line:
-                pShippingState = line.split("Shipping State: ")[1].strip()
-            if "Shipping Phone:" in line:
-                pShippingPhone = line.split("Shipping Phone: ")[1].strip()
+            # ... (other shipping variables)
 
             # Define billing variables from the selected profile.
             if "Billing Address:" in line:
                 pBillingAddress = line.split("Billing Address: ")[1].strip()
-            if "Billing Secondary:" in line:
-                pBillingSecondary = line.split("Billing Secondary: ")[1].strip()
-            if "Billing City:" in line:
-                pBillingCity = line.split("Billing City: ")[1].strip()
-            if "Billing Zip Code:" in line:
-                pBillingZipCode = line.split("Billing Zip Code: ")[1].strip()
-            if "Billing State:" in line:
-                pBillingState = line.split("Billing State: ")[1].strip()
-            if "Billing Phone:" in line:
-                pBillingPhone = line.split("Billing Phone: ")[1].strip()
+            # ... (other billing variables)
 
     os.system("cls")
     print("Your profile has been loaded.")
@@ -81,54 +60,110 @@ def profileSelect():
     loadSite()
 
 def ticketmasterAccountDefine():
-    global account
+    global account, email, password
+
+    # Read the accounts from the file and store them as tuples in a list
     with open("ticketmaster_accounts.txt", "r") as file:
         for line in file:
+            # Split the line into email and password
             email, password = line.strip().split(":")
+            account = (email, password)
             
-            account = f"{email}:{password}"
+            # Append the account to the list
             ticketmasterAccountList.append(account)
+
         for account in ticketmasterAccountList:
             print(account)
 
     os.system("cls")    
 
-    print("Accounts loaded. ")
+    print("Accounts loaded.")
     time.sleep(1)
-    os.system("cls")    
+    os.system("cls")
 
-    print("Ticketmaster module loading... ")
-    time.sleep(5)
+    print("Ticketmaster module loading...")
+    time.sleep(1)
     ticketmasterModule()
 
+def markAccountAsUsed():
+    # Mark an account as used by adding a '#' in front of it in the file
+    with open("ticketmaster_accounts.txt", "r") as file:
+        lines = file.readlines()
+    with open("ticketmaster_accounts.txt", "w") as file:
+        for line in lines:
+            if f"{email}:{password}" in line:
+                line = "#" + line
+            file.write(line)
+
+def getUnusedAccount():
+    for email, password in ticketmasterAccountList:
+        # Check if the account is not marked as used (doesn't start with '#')
+        if not email.startswith("#"):
+            return email, password
+    return None, None
+
 def ticketmasterLogin():
-    try:
-        time.sleep(5)
-        button = driver.find_element(By.ID, 'company-logo')
-        button.click()
-        print("Button clicked successfully. ")
+    global driver, actions
 
-    except Exception as e:
-        print("Failed to click the button: ", e)
+    email, password = getUnusedAccount()
 
-    try:
-        time.sleep(5)
-        email_input = driver.find_element(By.XPATH, "//input[@placeholder='email']")
-        
-        email_input.clear()
-        
-        email_input.send_keys("your_email@example.com")
-        
-        print("Email entered successfully.")
+    if email is not None:
+        try:
+            time.sleep(5)
+            button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.ID, 'company-logo'))
+            )
+            button.click()
+            print("Button clicked successfully.")
 
-    except Exception as e:
-        print("Failed to enter email: ", e)
+        except Exception as e:
+            print("Failed to click the button: ", e)
+
+        try:
+            time.sleep(10)
+
+            actions.send_keys(Keys.TAB).perform()
+            actions.send_keys(Keys.TAB).perform()
+            actions.send_keys(Keys.TAB).perform()
+
+            actions.send_keys(email).perform()
+    
+            actions.send_keys(Keys.TAB).perform()
+
+            actions.send_keys(password).perform()
+            
+            actions.send_keys(Keys.TAB).perform()
+            actions.send_keys(Keys.TAB).perform()
+            actions.send_keys(Keys.TAB).perform()
+            actions.send_keys(Keys.TAB).perform()
+            actions.send_keys(Keys.TAB).perform()
+            actions.send_keys(Keys.TAB).perform()
+            print("Details entered successfully.")
+
+            actions.send_keys(Keys.RETURN).perform()
+
+            time.sleep(5)
+
+            actions.send_keys(Keys.TAB).perform()
+            actions.send_keys(Keys.RETURN)
+
+            time.sleep(10)
+
+            actions.send_keys('').perform()
+
+            print("Anti-bot done successfully.")
+        except Exception as e:
+            print("Failed to enter email: ", e)
+
+    else:
+        print("No unused accounts available.")
 
     input("")
-
+    
+    markAccountAsUsed()
 
 def ticketmasterModule():
-    global driver
+    global driver, actions
 
     os.system("cls")
     ticketmasterSelected = input("What/who are we going for? ")
@@ -136,6 +171,8 @@ def ticketmasterModule():
 
     driver = webdriver.Chrome()
     driver.get('https://registration.ticketmaster.com/' + ticketmasterSelected)
+
+    actions = ActionChains(driver)
 
     os.system("cls") 
     print("Now loading " + driver.title + "...")
